cf2ef2932824901add10286dcabc70b89bc222a7
andy <andy.liujin@foxmail.com>
2020-05-22 14:31:31 +0000

    3&4 页面布局 & bootstrap & 浏览器缓存问题

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title', 'Weibo App')  - Laravel 新手入门教程</title>
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+  @include('layouts._header')
+  <div class="container">
+    @yield('content')
+    @include('layouts._footer')
+  </div>
+</body>
+</html>
